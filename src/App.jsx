465e77737876ee72bad839940587414bcfb24069
chamok barmon <chamokbarmon123@gmail.com>
2025-09-29 19:59:14 +0600

    thirt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 
 import "./App.css";
 import Collapse from "./Collapse/Collapse";
+import Footer from "./Footer/Footer";
 import Hero from "./Hero/Hero";
 
 function App() {
 
   return (
     <>
+    /* Collapse Section */
+    
      <Collapse></Collapse>
+
+      /* Hero Section */
+
      <Hero></Hero>
+
+      /* Footer Section */
+
+     <Footer></Footer>
     </>
   );
 }
--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,18 +1,7 @@
-import "./Hero.css";
+import { ArrowRightIcon } from "@heroicons/react/16/solid";
 
 const Hero = () => {
   return (
-    // <div className=" bgColor text-center w-[1360px] ">
-    //   <div >
-    //     <h1>Get In Touch</h1>
-    //     <p>
-    //       Contact us now to enquire our plumbing services, whether you have a
-    //       commercial project that requires support, or a domestic plumbing task
-    //       that needs the attention of a trusted professional.
-    //     </p>
-    //     <button>Book a Professional Plumber</button>
-    //   </div>
-    // </div>
     <div className="w-full flex justify-center">
       <div className="hero h-[405px] bg-[#0058FF] w-[1200px] rounded">
         <div className="hero-content text-center">
@@ -20,13 +9,14 @@ const Hero = () => {
             <h1 className="text-5xl font-bold text-[24px] md:text-[32px] lg:text-[48px] lg:font-[inter] ">
               Get In Touch
             </h1>
-            <p className="py-6">
+            <p className="py-6 xl:text-[18px] md:text-[14px] text-[14px]">
               Contact us now to enquire our plumbing services, whether you have
               a commercial project that requires support, or a domestic plumbing
               task that needs the attention of a trusted professional.
             </p>
-            <button className="btn btn-primary">
+            <button className="btn btn-primary bg-[white] text-[#0058FF]  xl:text-[16px] sm:text-[10px]">
               Book a Professional Plumber
+              <ArrowRightIcon className="h-5 w-5" />
             </button>
           </div>
         </div>
